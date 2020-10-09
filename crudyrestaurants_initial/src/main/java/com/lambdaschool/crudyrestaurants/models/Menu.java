5f13ef6ba8922bd760ad2f8865b2b958215e4ac7
donavynhaley <donavynhaley@gmail.com>
2020-10-08 21:05:51 -0700

    Halfway

---
+++ crudyrestaurants_initial/src/main/java/com/lambdaschool/crudyrestaurants/models/Menu.java
@@ -0,0 +1,61 @@
+package com.lambdaschool.crudyrestaurants.models;
+
+import javax.persistence.*;
+
+// 1:03
+@Entity
+@Table(name = "menus")
+public class Menu
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long menuid;
+
+    @Column(nullable = false)
+    private String dish;
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "resturantid", nullable = false)
+    private Restaurant restaurant;
+
+
+    public Menu()
+    {
+    }
+
+    public Menu(String dish, double price)
+    {
+        this.dish = dish;
+        this.price = price;
+    }
+
+    public long getMenuid()
+    {
+        return menuid;
+    }
+
+    public void setMenuid(long menuid)
+    {
+        this.menuid = menuid;
+    }
+
+    public String getDish()
+    {
+        return dish;
+    }
+
+    public void setDish(String dish)
+    {
+        this.dish = dish;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
+}
--- crudyrestaurants_initial/src/main/java/com/lambdaschool/crudyrestaurants/models/Restaurant.java
+++ crudyrestaurants_initial/src/main/java/com/lambdaschool/crudyrestaurants/models/Restaurant.java
@@ -1,11 +1,8 @@
 package com.lambdaschool.crudyrestaurants.models;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The entity allowing interaction with the restaurants table.
@@ -53,6 +50,11 @@ public class Restaurant
      * This was added to specifically show how to update fields that do not have a NULL value.
      */
     private int seatcapacity;
+
+    @OneToMany(mappedBy = "restaurant",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true )
+    private List<Menu> menus = new ArrayList<>();
 
     /**
      * Default constructor used primarily by the JPA.
@@ -229,4 +231,15 @@ public class Restaurant
     {
         this.seatcapacity = seatcapacity;
     }
+
+    public List<Menu> getMenus()
+    {
+        return menus;
+    }
+
+    public void setMenus(List<Menu> menus)
+    {
+        this.menus = menus;
+    }
 }
+
